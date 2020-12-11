46fcf85a15a475a8df6a1cb35b497e3b6554047e
jhonatantm9 <64021727+jhonatantm9@users.noreply.github.com>
2020-12-10 23:56:18 -0500

    Add files via upload

---
+++ Pagina html/js/botones-informacion.js
@@ -0,0 +1,21 @@
+//Funciones para cada uno de los botones de las páginas, us función es abrir el link en una pestaña nueva
+
+function guatapeInfo(){
+    window.open("https://colombia.travel/es/medellin/guatape-y-piedra-del-penol");
+}
+
+function jardinInfo(){
+    window.open("https://colombia.travel/es/jardin-antioquia");
+}
+
+function jericoInfo(){
+    window.open("https://colombia.travel/es/jerico");
+}
+
+function santaFeInfo(){
+    window.open("https://colombia.travel/es/santa-fe-de-antioquia");
+}
+
+function sanCarlosInfo(){
+    window.open("http://puebliandoporantioquia.com.co/subregion-oriente/municipio-san-carlos/");
+}
